ad48590a7c5f0969561db91c64e9c0d914244331
ArvidUnell <arun06001@utb.vaxjo.se>
2024-04-10 13:30:51 +0200

    Add project files.

---
+++ Uppgift_8.2-4/Person.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_8._2_4
+{
+    internal class Person
+    {
+        private string förnamn;
+        private string efternamn;
+        public string Förnamn
+        {
+            get
+            {
+                return förnamn;
+            }
+            set
+            {
+                förnamn = value;
+            }
+        }
+        public string Efternamn
+        {
+            get
+            {
+                return efternamn;
+            }
+            set
+            {
+                efternamn = value;
+            }
+        }
+
+        public string FulltNamn()
+        {
+            return förnamn + " " + efternamn;
+        }
+
+
+        private DateTime födelsedatum;
+        public DateTime Födelsedatum
+        {
+            get
+            {
+                return födelsedatum;
+            }
+            set
+            {
+                födelsedatum = value;
+            }
+        }
+        public bool ÄrMyndig()
+        {
+            if (DateTime.Now.Year - födelsedatum.Year < 18)
+            {
+                return false;
+            }
+            if (DateTime.Now.Year - födelsedatum.Year > 18)
+            {
+                return true;
+            }
+            return DateTime.Now.DayOfYear >= födelsedatum.DayOfYear;
+        }
+
+
+        private float längd;
+        public float Längd
+        {
+            get
+            {
+                return längd;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    längd = 0;
+                }
+                else
+                {
+                    längd = value;
+                }
+            }
+        }
+
+        private int vikt;
+        public int Vikt
+        {
+            get
+            {
+                return vikt;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    vikt = 0;
+                }
+                else
+                {
+                    vikt = value;
+                }
+            }
+        }
+
+        public float BMI()
+        {
+            return vikt / (längd * längd);
+        }
+    }
+}
---
+++ Uppgift_8.2-4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Uppgift_8._2_4;
+
+namespace Uppgift_8_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Förnamn = "Olle";
+            p1.Efternamn = "Wessman";
+            Console.WriteLine("Fulla namnet: " + p1.FulltNamn());
+
+            p1.Födelsedatum = new DateTime(2005, 4, 29);
+            Console.WriteLine($"Hen är {(p1.ÄrMyndig() ? "" : "inte ")}myndig");
+
+            p1.Längd = 1.73f;
+            p1.Vikt = 69;
+            Console.WriteLine($"Hens BMI är {p1.BMI()}.");
+        }
+    }
+}
